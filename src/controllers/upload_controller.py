bf17c8b8a26e1b29bcb5192404520cb94cff3b42
Emanuel Carricio <emanuelferreira.aluno@unipampa.edu.br>
2025-12-12 19:45:20 -0300

    feat: Implementa endpoint de upload com validação e processamento (Task 3)

--- src/controllers/upload_controller.py
+++ src/controllers/upload_controller.py
@@ -1,24 +1,30 @@
 from fastapi import APIRouter, UploadFile, File, HTTPException
+from src.services.data_processor import process_dataset
 
 router = APIRouter()
 
-@router.post("/upload/", tags=["Upload"])
-async def upload_file(file: UploadFile = File (...)):
-  """
-  Recebe um arquivo CSV ou XLSX para processamento.
-  """
-  # Validar se o arquivo existe
-  if not file:
-    raise HTTPException(status_code=400, detail="Nenhum arquivo foi enviado.")
-  
-  #validar formato 
-  filename = file.filename.lower()
-  if not filename.endswith(('.csv', '.xlsx')):
-    raise HTTPException(status_code=400, detail="Formato de arquivo inválido. Apenas CSV e XLSX são permitidos.")
-  
-  return {
-    "status": "recebido",
-    "filename": file.filename,
-    "content_type": file.content_type,
-    "mensagem": "Arquivo recebido com sucesso para processamento."
-  }
+@router.post("/upload", tags=["Upload"])
+async def upload_file(file: UploadFile = File(...)):
+    """
+    Recebe arquivo, valida colunas, limpa dados e retorna estatísticas.
+    """
+    try:
+        content = await file.read()
+
+        # Processamento dos dados (task2)
+        # Se tiver colunas erradas, o process_dataset vai dar um erro 400 aqui
+        df_limpo = process_dataset(content, file.filename)
+
+        return {
+            "status": "sucesso",
+            "mensagem": "Arquivo validado e processado com sucesso!",
+            "total_linhas_validas": len(df_limpo),
+            "preview_dados": df_limpo.head(3).to_dict(orient="records")
+        }
+
+    except HTTPException as he:
+        # Se o data_processor lançou erro (ex: Coluna faltando), repassamos pro usuário
+        raise he
+    except Exception as e:
+        # Erros inesperados
+        raise HTTPException(status_code=500, detail=f"Erro interno no servidor: {str(e)}")
